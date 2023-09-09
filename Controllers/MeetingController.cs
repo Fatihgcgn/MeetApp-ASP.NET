cd981b43ea98591951650ceee92e45eff8618595
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-09 18:35:39 +0300

    Update Post method
    Update Post method

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -10,14 +10,26 @@ namespace MeetApp.Controllers
 {
     public class MeetingController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Apply(string Name,string Phone,string Email,bool WillAttend)
+        {
+            // Console.WriteLine(Name);
+            // Console.WriteLine(Phone);
+            // Console.WriteLine(Email);
+            // Console.WriteLine(WillAttend);
+            return View();
+        }
+        [HttpGet]
         public IActionResult List()
         {
             return View();
